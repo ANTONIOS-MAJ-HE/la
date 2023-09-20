ed36ae50dcccaecf547de9f754a1b2d6788435f9
ANTONIOS-MAJ-HE <95696217+ANTONIOS-MAJ-HE@users.noreply.github.com>
2023-09-19 23:30:27 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ const server = http.createServer(app)
 const {Server} = require('socket.io')
 const io = new Server(server)
 
+const path = require('path');
 app.use(express.static('plantillas'));
 
-io.on('connection', (socket) => {   socket.on('chat', (msg) => {
+io.on('connection', (socket) => {
+    // Procedimiento 4:
+   socket.on('chat', (msg) => {
         io.emit('chat', msg)
     })
 })
@@ -17,7 +20,7 @@ io.on('connection', (socket) => {   socket.on('chat', (msg) => {
 
 app.get('/', (req, resp) => {
     resp.sendFile(`${__dirname}/plantillas/chat_view.html`)
-})
+})//__dirname cada peticion envia el index .html
 
 server.listen(3200,() => {
     console.log('Servidor corriendo en http://localhost:3200')
